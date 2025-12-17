09bc2ef2d7c6b23bd714cfb7fe04e3cb46a7392d
Ayush Vardhan Jha <ayushvjha@gmail.com>
2025-12-17 21:13:05 +0530

    Add files via upload

---
+++ assignment1_Task1.py
@@ -0,0 +1,16 @@
+
+#This is assignment1 Task1 of Tutedude
+#This script takes two numbers as inputs from the user, and returns their sum, difference, product and division as output.
+
+first_num= float(input("Enter the first number "))
+second_num=float(input("Enter the second number "))
+
+add= first_num + second_num
+sub= first_num-second_num
+mul= first_num*second_num
+div=first_num/second_num
+
+print("The sum of ",first_num , "and", second_num, "is", add)
+print("The difference of ", first_num,"and", second_num,"is", sub)
+print("The product of ", first_num,"and", second_num,"is", mul)
+print("The division of ", first_num,"and", second_num,"is", div)
---
+++ assignment1_Task2.py
@@ -0,0 +1,8 @@
+
+#This is task 2 of assignment 1 by Tutedude
+#This script takes first name and last name as input from the user and prints the full name with a greetings message on the console.
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print("Hello, "+full_name+"!! Welcome to Python by Tutedude")
